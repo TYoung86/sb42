9faed96ae9b6e9892f617db026fa533d082be90c
Tyler Young <tyler.young@cimplx.com>
2016-09-03 23:31:58 -0400

    set content type a different way?

--- app/worker.js
+++ app/worker.js
@@ -176,7 +176,7 @@ http.createServer((req, res) => {
 	var proof = insecureResponses[req.url];
 	if (proof) {
 		console.log('Challenge request: %s', proof);
-		res.set('Content-Type', 'text/plain');
+		res.contentType('text/plain');
 		res.send(new Buffer(proof));
 		res.end();
 	} else {
@@ -245,7 +245,7 @@ app.use('/public', express.static('public'));
 
 app.get('/robots.txt',
 	(req, res) => {
-		res.set('Content-Type', 'text/plain');
+		res.contentType('text/plain');
 		res.send(new Buffer(insecureResponses['/robots.txt']));
 	});
 
